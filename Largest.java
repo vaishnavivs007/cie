3cf8346b17d3a29e98d4d9c12df888b7beaaea67
vaishnavivs007 <vaishnavishejwadkar@gmail.com>
2025-04-04 10:44:07 +0530

    save the java file

---
+++ Largest.java
@@ -0,0 +1,16 @@
+public class Largest{
+     public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 50;
+        if (a>b){
+            System.out.println("a is greater than b" +(a>b));
+        }
+        else if (a>cb){
+            System.out.println("a is greater than c" +(a>c));
+        }
+        else{
+            System.out.println("c is greater than a and b");
+        }
+     }
+}
